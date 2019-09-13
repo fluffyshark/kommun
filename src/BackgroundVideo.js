88843221a8fbdeaa4f8b0a88a9d2e2de0b459334
Robin <robin.yjord1@gmail.com>
2019-09-13 06:01:01 +0200

    Added-welcome-animations

--- src/BackgroundVideo.js
+++ src/BackgroundVideo.js
@@ -3,6 +3,7 @@ import background from './img/background.jpg';
 import Header from './Header_menu';
 import welcome from './img/welcome.png';
 import './css/background.css'
+import {Spring} from 'react-spring/renderprops'
 
 class BackgroundVideo extends React.Component {
   render() {
@@ -15,7 +16,19 @@ class BackgroundVideo extends React.Component {
       <div className="container" style={{ width:windowWidth, height:windowHeight}}>
           <img style={{width:windowWidth}} src={background} alt="background" />
           <div className="background_image_position">
-            <img className="background_welcome_image" src={welcome} alt="welcome" />
+
+          <Spring
+            from={{ opacity: 0, marginTop: -30 }}
+            to={{ opacity: 1, marginTop: 0 }}
+            config={{ delay:500, duration:500 }}
+          >
+            { props => (
+              <div style={props}>
+              <img className="background_welcome_image" src={welcome} alt="welcome" />
+              </div>
+            ) }
+
+            </Spring>
           </div>
           <Header />
       </div>
